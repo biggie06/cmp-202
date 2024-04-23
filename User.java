1834c93d1f3008d01adbcecb61f8fa4f19cd2876
JOSEPH AGWUNGWU <joseph.agwungwu@binghamuni.edu.ng>
2024-04-23 13:42:18 +0100

    Initial commit

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User {
+    String username ="Bigjoe";
+    String password = "bamdmanjoe";
+    String login() {
+    if(username.equals("Bigjoe") && password.equals("badmanjoe")){
+        return "success";
+    } else {
+        return "failed";
+    }
+}
+    public static void main (String[] args){
+        User joe = new User();
+        joe.login();
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,7 @@
+public class Variable {
+    public static void main(String[] args) {
+        int x =2000;
+        System.out.println(x);
+
+    }
+}
